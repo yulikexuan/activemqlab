5a0a061036638a70f68943b6d474d71cb0a53b4b
yulikexuan <yulikexuan@gmail.com>
2019-01-10 20:34:04 -0500

    Initial commit

---
+++ src/main/java/com/yuli/activemqlab/ActivemqlabApplication.java
@@ -0,0 +1,14 @@
+package com.yuli.activemqlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqlabApplication.class, args);
+	}
+
+}
+
