e251b8b319317f9f36af85f44f5e0707d8683d21
yulikexuan <yulikexuan@gmail.com>
2019-01-10 21:03:34 -0500

    Init activemq

--- src/test/java/com/yuli/activemqlab/ActivemqlabApplicationIT.java
+++ src/test/java/com/yuli/activemqlab/ActivemqlabApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ActivemqlabApplicationTests {
+public class ActivemqlabApplicationIT {
 
 	@Test
 	public void contextLoads() {
